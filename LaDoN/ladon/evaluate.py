6930f586ec2da115a2d9df4e83b008d1b6781023
Mikkel Werling <werling1407@gmail.com>
2022-04-09 17:49:26 -0700

    parameter importance and report

--- LaDoN/ladon/evaluate.py
+++ LaDoN/ladon/evaluate.py
@@ -3,6 +3,10 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.io as pio
+import optuna
+import joblib
+from config import NAME_DICTIONARY
 
 sns.set(rc={"figure.figsize": (11.7, 8.27)})
 sns.set_context("talk")
@@ -125,3 +129,76 @@ g = sns.barplot(
 g.set(xlabel="Mean Difference", ylabel="")
 plt.legend(title="Network", bbox_to_anchor=(0.5, 0.5, 0.52, 0.52))
 plt.savefig("plots/overall/Model_Evaluation.png", dpi=300, bbox_inches="tight")
+
+pio.renderers.default = "notebook"
+
+
+def get_important_parameters(network):
+    study = joblib.load(f"analysis/data/optimization/{network}_study.pkl")
+    dict_of_values = dict(optuna.importance.get_param_importances(study))
+    dict_of_values["network"] = network
+    return dict_of_values
+
+
+def rename_value_names(string):
+    old_names = [
+        "randomness",
+        "tie_dissolution",
+        "threshold",
+        "negative_learning_rate",
+        "positive_learning_rate",
+    ]
+    new_names = [r"$R$", r"$P(D)$", r"$T$", r"$\beta$", r"$\alpha$"]
+    translation = {old_names[i]: new_names[i] for i in range(len(old_names))}
+    return translation.get(string)
+
+
+list_of_parameters = [get_important_parameters(network) for network in NAME_DICTIONARY]
+
+importance_df = pd.DataFrame(list_of_parameters)
+
+importance_df = importance_df.melt(id_vars="network")
+
+importance_df["variable"] = importance_df["variable"].apply(
+    lambda x: rename_value_names(x)
+)
+importance_df["network"] = importance_df["network"].apply(
+    lambda x: change_network_labels(x)
+)
+
+sns.barplot(
+    data=importance_df,
+    x="variable",
+    y="value",
+    hue="network",
+    palette="muted",
+    hue_order=[
+        "Karate Club",
+        "Dolphins",
+        "Citation Network",
+        "Political Books",
+        "Political Blogs",
+        "Politicians",
+    ],
+).set(
+    ylabel="Parameter Importance",
+    xlabel="",
+)
+plt.legend(title=r"Network")
+plt.savefig(
+    "plots/overall/Parameter_Importance.png",
+    dpi=300,
+    bbox_inches="tight",
+)
+
+fig = optuna.visualization.plot_param_importances(study)
+fig.show()
+
+fig = optuna.visualization.plot_optimization_history(study)
+fig.show()
+
+fig = optuna.visualization.plot_edf(study)
+fig.show()
+
+fig = optuna.visualization.plot_slice(study)
+fig.show()
